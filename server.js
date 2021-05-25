bb32f30f8761ab6ac195d5a86bf77ef066826e59
ariellongoria <ariello97@hotmail.com>
2021-05-24 20:40:22 -0500

    idk

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-app.get("./api/animals", (req, res) => {
+app.get("/api/animals", (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
@@ -41,5 +41,5 @@ app.get("./api/animals", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
 });
